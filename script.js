bcdae34e9638de124bcf8096719cc44b9d8b8128
allal83 <141268779+allal83@users.noreply.github.com>
2023-08-28 15:54:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const option1 = document.getElementById('option1');
+const option2 = document.getElementById('option2');
+
+option1.addEventListener('click', () => {
+    alert('Você escolheu a Opção 1!');
+});
+
+option2.addEventListener('click', () => {
+    alert('Você escolheu a Opção 2!');
+});
